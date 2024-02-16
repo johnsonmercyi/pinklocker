d45e56975c8244b65b0eb03b74ebd0501181cdcc
johnsonmercyi <50102624+johnsonmercyi@users.noreply.github.com>
2024-02-16 06:51:59 +0100

    wrapped app/layout children with the  Web3ModalProvider

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -3,6 +3,7 @@ import './css/style.css'
 import { Inter } from 'next/font/google'
 import Theme from './theme-provider'
 import AppProvider from './app-provider'
+import { Web3ModalProvider } from "@/app/(default)/tokens/config/Web3ModalConfig";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -24,9 +25,11 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>{/* suppressHydrationWarning: https://github.com/vercel/next.js/issues/44343 */}
       <body className={`${inter.variable} font-inter antialiased bg-slate-100 dark:bg-slate-900 text-slate-600 dark:text-slate-400`}>
         <Theme>
-          <AppProvider>
-            {children}
-          </AppProvider>
+          <Web3ModalProvider>
+            <AppProvider>
+              {children}
+            </AppProvider>
+          </Web3ModalProvider>
         </Theme>
       </body>
     </html>
